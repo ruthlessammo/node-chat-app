f2dafd2a046631fa238ea0c7b6eca77599ede6e0
ruthlessammo <williamjchilton@gmail.com>
2017-12-21 16:59:53 -0500

    think heroku bug fixed due to port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ let app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server is up and spitting bars to Andre ${port}`);
 });
 module.exports.app = app;
