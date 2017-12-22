46ba81211bd9099e497d742629de8b06a35b2b61
ruthlessammo <williamjchilton@gmail.com>
2017-12-21 19:01:25 -0500

    added http and socketio

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 let app = express();
+let server = http.createServer(app)
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up and spitting bars to Andre ${port}`);
 });
 module.exports.app = app;
