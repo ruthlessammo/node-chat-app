6c5d565ea8368b23509b4e45103e76be92c035b9
ruthlessammo <williamjchilton@gmail.com>
2017-12-27 14:35:51 -0500

    added test for geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should gen the correct message object', () => {
@@ -9,5 +9,18 @@ describe('generateMessage', () => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Will';
+    const latitude = 15;
+    const longitude = 12;
+    const url = `http://www.google.com/maps?q=15,12`;
+    const message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
