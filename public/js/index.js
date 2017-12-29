952caaac0d51db3320d127ace262bfd618bf12ec
ruthlessammo <williamjchilton@gmail.com>
2017-12-29 16:43:54 -0500

    added styles and better ui

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@ socket.on('newLocationMessage', function(message) {
 
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
+  let messageTextbox = $('[name=message]')
   socket.emit('createMessage', {
     from: 'User',
     text: $('[name=message]').val()
   }, function() {
-
+    messageTextbox.val('');
   });
 });
 
@@ -43,12 +44,16 @@ locationButton.on('click', function() {
     return alert('Geolocation not supported by your browser.');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Share location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function() {
+    locationButton.removeAttr('disabled').text('Share location');
     alert('Unable to fetch location')
   });
 });
